e5423dc4a0102b631140c83f7707c1718eea6178
John Pane <wabanichi@gmail.com>
2018-08-14 19:47:38 -0500

    bullshit

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+module.exports = Phrase;
+
 // Adds `reverse` to all strings
 String.prototype.reverse = function() {
 	return Array.from(this).reverse().join("");
@@ -10,6 +12,17 @@ function Phrase(content) {
   // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
   	return this.content.toLowerCase();
+  }
+
+  //Returns the letters in the content.
+  this.letters = function letters() {
+    let theLetters = [];
+    for (let i = 0; i < this.content.length; i++) {
+      if (this.content.chartAt(i).match(/[a-zA-Z]/)) {
+        theLetters.push(this.content.chartAt(i));
+      }
+    }
+    return theLetters.join("");
   }
 
   //Returns true if the phrase is a palindrome
